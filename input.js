76f1817f73e6852efcdc3b1f459e24f8d4c7deb1
Jehanne Hodge <samarahjhodge@gmail.com>
2020-06-03 18:33:30 -0400

    use wasd keys for movement

--- input.js
+++ input.js
@@ -1,4 +1,7 @@
-const setupInput = function() {
+let connection;
+
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
@@ -6,7 +9,7 @@ const setupInput = function() {
   
   stdin.on('data', (key) => {
     handleUserInput(key);
-  })
+  });
   return stdin;
 };
 
@@ -14,6 +17,19 @@ const handleUserInput = (key) => {
   if (key === '\u0003') {
     process.exit();
   }
+  if (key === 'w') {
+    connection.write("Move: up")
+  }
+  if (key === 's') {
+    connection.write("Move: down")
+  }
+  if (key === 'a') {
+    connection.write("Move: left")
+  }
+  if (key === 'd') {
+    connection.write("Move: right")
+  }
 };
+
 
 module.exports = { setupInput };
