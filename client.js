46b0d334bd04092809ee826d30bcd10fa0039366
Jehanne Hodge <samarahjhodge@gmail.com>
2020-06-03 14:20:17 -0400

    add name to to snake as message

--- client.js
+++ client.js
@@ -6,10 +6,19 @@ const net = require('net');
     host: '135.23.222.148',
     port: 50541
   });
+
   conn.on('data', (data) => {
     console.log('Server says: ', data);
   });
-  
+
+  conn.on('connect', () => {
+    console.log('sucessfully connected to game server')
+  });
+
+  conn.on('connect', () => {
+    conn.write('Name: JEH')
+  });
+
   // interpret incoming data as text
   conn.setEncoding('utf8'); 
 
